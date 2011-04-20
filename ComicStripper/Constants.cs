cf542506d47c0ab6e2c2caad422a7189795b197c
Adam Nofsinger <github@anofsinger.com>
2011-04-20 08:20:55 -0500

    worked to point it makes web requests for the page, uses regex, and does HEAD

---
+++ ComicStripper/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComicStripper
+{
+    public static class Constants
+    {
+        public const string EmailTemplate = @"<font face='Arial,sans serif'><p><b>[[TITLE]]</b><br> 
+<a href='[[LINK]]' target='_new'>
+<img style='border:1;border-color:black;border-style:solid' src='cid:[[IMAGE]]'
+title='[[ALTTEXT]]' alt='[[TITLE]]' ></a></p><br>[[ALTTEXT]]";
+
+        public const string ComicsFile = "Comics.txt";
+
+        public struct Sections
+        {
+            public const string Regex = "regex";
+            public const string Comics = "comics";
+        }
+    }
+}
