62496d0e3b7e8517af423a7fb82bddb6126f8566
Adam Nofsinger <github@anofsinger.com>
2011-04-25 10:30:10 -0500

    progress on XML serialization

---
+++ ComicStripper/Util/XmlEx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace Util
+{
+    public static class XmlEx
+    {
+        public static void ToXmlFile<T>(this T source, string filePath)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(T));
+            using (FileStream fs = File.OpenWrite(filePath))
+            {
+                s.Serialize(fs, source);
+            }
+        }
+
+        public static T FromXmlFile<T>(string filePath)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(T));
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                T source = (T)s.Deserialize(fs);
+                return source;
+            }
+        }
+    }
+}
