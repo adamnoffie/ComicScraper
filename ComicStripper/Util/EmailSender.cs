45b4e6d22c72e4542516f7b91c4dc527e9f8ce89
Adam Nofsinger <github@anofsinger.com>
2011-04-29 13:30:29 -0500

    Ooops, missed some files

---
+++ ComicStripper/Util/EmailSender.cs
@@ -0,0 +1,125 @@
+﻿// ----------------------------------------------------------------------------
+// EmailSender.cs
+// Copyright (c) 2009 Adam Nofsinger <adam.nofsinger@gmail.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// ----------------------------------------------------------------------------
+using System;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net.Configuration;
+
+namespace Util
+{
+    /// <summary>
+    /// Utility class for sending emails from .NET applications.
+    /// <remarks>Depends on System.Configuration</remarks>
+    /// </summary>
+    public static class EmailSender
+    {
+        /// <summary>
+        /// Send an email using server SMTP settings from Web.config
+        /// </summary>
+        /// <param name="from">FROM field of email</param>
+        /// <param name="to">TO field of email</param>
+        /// <param name="replyto">REPLY-TO field of email</param>
+        /// <param name="subject">The subject of the email message</param>
+        /// <param name="body">The body of the email message</param>
+        /// <param name="isHTML">Is the body HTML encoded</param>
+        /// <param name="useSSL">use SSL when connecting to SMTP server?</param>
+        public static void SendEmail(MailAddress from, MailAddress to, MailAddress replyto,
+            string subject, string body, bool isHTML, bool useSSL)
+        {
+            MailMessage mesg = new MailMessage(from, to);
+            mesg.Subject = subject;
+            mesg.IsBodyHtml = isHTML;
+            mesg.Body = body;
+            if (replyto != null)
+                mesg.ReplyTo = replyto;
+
+            // send the email
+            SmtpClient smtp = new SmtpClient();     // pulls smtp settings from Web.config
+            smtp.EnableSsl = useSSL;
+            smtp.Send(mesg);
+        }
+
+        /// <summary>
+        /// Sends an email using the SMTP settings stored in the Web.Config file
+        /// </summary>
+        /// <param name="from">from address of email</param>
+        /// <param name="fromDisplayName">display name of from address</param>
+        /// <param name="to">who to send email to - this can be multiple addresses separated by semi-colon (;) or commma (,)</param>
+        /// <param name="replyto">The replyto address used, leave as string.Empty to not use a reply-to address</param>
+        /// <param name="replytoDisplayName">display name of the replyto address</param>
+        /// <param name="subject">subject of the email message</param>
+        /// <param name="body">body of the email</param>
+        /// <param name="isHTML">is the body HTML? - plain text otherwise</param>
+        /// <param name="useSSL">use SSL to connect to the SMTP server?</param>
+        public static void SendEmail(string from, string fromDisplayName, string to,
+            string replyto, string replytoDisplayName, string subject, string body, bool isHTML, bool useSSL)
+        {
+            MailAddress fromAddress = new MailAddress(from, fromDisplayName);
+            MailAddress replyAddress = string.IsNullOrEmpty(replyto) ?
+                null : new MailAddress(replyto, replytoDisplayName);
+
+            // send out an email for each address in the to string
+            string[] tos = to.Replace(" ", string.Empty).Split(',', ';');
+            foreach (string address in tos)
+            {
+                MailAddress toAddress = new MailAddress(address);
+                SendEmail(fromAddress, toAddress, replyAddress, subject, body, isHTML, useSSL);
+            }
+        }
+
+        /// <summary>
+        /// Sends an email using the SMTP settings stored in app config file.
+        /// </summary>
+        /// <param name="msg">Must have the body or AlternateViews set</param>
+        /// <param name="to">who to send email to - this can be multiple addresses separated by semi-colon (;) or commma (,)</param>
+        /// <param name="subject">email message subject</param>
+        /// <param name="useSSL">use SSL to connect to SMTP server?</param>
+        public static void SendEmail(MailMessage msg, string to, string subject, bool useSSL)
+        {
+            msg.From = new MailAddress(GetFromEmailAddressFromConfig());
+            msg.Subject = subject;
+
+            // send out an email for each address in the to string
+            string[] tos = to.Split(',', ';');
+            foreach (string address in tos)
+                msg.To.Add(new MailAddress(address.Trim()));
+
+            SmtpClient smtp = new SmtpClient(); // uses smtp settings from app config
+            smtp.EnableSsl = useSSL;
+            smtp.Send(msg);
+        }
+
+        /// <summary>
+        /// Returns web.config::system.net/mailSettings/smtp/from or 
+        /// web.config::system.net/mailSettings/smtp/network/username if the first is not specified
+        /// </summary>
+        /// <returns></returns>
+        public static string GetFromEmailAddressFromConfig()
+        {
+            // Get the configuration <mailSettings> element
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            MailSettingsSectionGroup settings = 
+                (MailSettingsSectionGroup)config.GetSectionGroup("system.net/mailSettings");
+            
+            // return the from email address, or if it is empty, return the network username
+            if (!string.IsNullOrEmpty(settings.Smtp.From))
+                return settings.Smtp.From;
+            else
+                return settings.Smtp.Network.UserName;
+        }
+    }
+}
---
+++ ComicStripper/Util/HttpFetch.cs
@@ -0,0 +1,115 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Util;
+
+namespace Util
+{
+    public static class HttpFetch
+    {
+        /// <summary>
+        /// Do an Http GET of a url, and store results to file. Extension is determined by content type.
+        /// </summary>
+        /// <param name="uri">uri of object to fetch</param>
+        /// <param name="filePath">file path to store fetched object at (this method will try to determine the correct file extension)</param>
+        /// <param name="userAgent">http UserAgent header</param>
+        /// <param name="referer">http Referer header</param>
+        /// <returns></returns>
+        public static string UrlToFile(Uri uri, string filePath, string userAgent, string referer)
+        {
+            // TODO: try catch
+
+            HttpWebRequest req = WebRequest.Create(uri) as HttpWebRequest;
+            req.Method = "GET";
+            req.UserAgent = userAgent;
+            req.Referer = referer;
+            using (WebResponse resp = req.GetResponse())
+            using (Stream responseStream = resp.GetResponseStream())
+            {
+                // file extension
+                string ext = ImageType.GetExtension(resp.ContentType);
+                filePath += "." + ext;
+
+                using (FileStream fs = File.Create(filePath))
+                {
+                    // read 32K of the stream at a time, writing to file
+                    Byte[] buffer = new Byte[32 * 1024];
+                    int read = responseStream.Read(buffer, 0, buffer.Length);
+                    while (read > 0)
+                    {
+                        fs.Write(buffer, 0, read);
+                        read = responseStream.Read(buffer, 0, buffer.Length);
+                    }
+                }
+
+                return filePath;
+            }
+        }
+
+        /// <summary>
+        /// Do an Http GET request of a url, and return the results as a string (good for HTML pages)
+        /// </summary>
+        /// <param name="url">url of object to fetch</param>
+        /// <param name="userAgent">UserAgent http header</param>
+        /// <returns></returns>
+        public static string UrlAsString(string url, string userAgent)
+        {
+            // TODO: try catch, return null or string.Emtpy on error
+
+            //
+            // TODO: http://www.google.com/search?aq=f&sourceid=chrome&ie=UTF-8&q=WebRequest+C%23+read+page+as+string
+            //      Make this more reliable using maybe a byte[] too, since that seems to be more reliable
+            //
+
+            HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+            req.Method = "GET";
+            req.UserAgent = userAgent;
+            using (WebResponse resp = req.GetResponse())
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+            {
+                
+
+
+                return sr.ReadToEnd();  // THIS STALLS SOMETIMES???
+
+                //// THIS DOESN'T GET EVERYTHING ON SOME REQUESTS
+                //StringWriter sw = new StringWriter();
+                //char[] buffer = new char[32 * 1024];
+                //int read = sr.Read(buffer, 0, buffer.Length);
+                //while (read > 0)
+                //{
+                //    sw.Write(buffer);
+                //    read = sr.Read(buffer, 0, buffer.Length);
+                //}
+
+                //return sw.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Do an Http HEAD request to get the size of an object without fetching it
+        /// </summary>
+        /// <param name="uri">url to the resource</param>
+        /// <param name="referer">Referer http header</param>
+        /// <param name="userAgent">UserAgent http header</param>
+        /// <returns></returns>
+        public static long UrlContentSize(Uri uri, string userAgent, string referer)
+        {
+            // TODO: try catch
+
+            HttpWebRequest req = WebRequest.Create(uri) as HttpWebRequest;
+            req.Method = "HEAD";
+            req.UserAgent = userAgent;
+            req.Referer = referer;
+            using (WebResponse resp = req.GetResponse())
+            {
+                return resp.ContentLength;
+            }
+        }
+
+    }
+}
